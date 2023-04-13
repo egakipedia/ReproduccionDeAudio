2c5e5f3bead1f23c68f1e262991886b548a5da2f
egakipedia <juancarlos.roldan@egaclm.com>
2023-04-13 19:40:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/reproducciondeaudio/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.reproducciondeaudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Clase SoundPool
+    Button play;
+    SoundPool sp;
+    int sonido_de_Reproduccion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Clase SoundPool
+        play = (Button) findViewById(R.id.btn_play);
+        sp = new SoundPool(1, AudioManager.STREAM_MUSIC, 1);
+        sonido_de_Reproduccion = sp.load(this, R.raw.sound_short, 1);
+    }
+
+    public void AudioSoundPool(View view){
+        sp.play(sonido_de_Reproduccion, 1,1,1, 0, 1);
+    }
+
+    // Método para MediaPlayer
+    public void AudioMediaPlayer(View view){
+       MediaPlayer mp = MediaPlayer.create(this, R.raw.sound_long);
+       mp.start();
+    }
+}
